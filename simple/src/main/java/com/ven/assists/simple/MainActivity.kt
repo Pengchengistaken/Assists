2eea52e4d19062fc2edd77f0946b2a31a09a04b4
Li Pengcheng <lipc@giec.cn>
2025-05-21 23:18:00 +0800

    refactor(main): remove unused variables and import

--- simple/src/main/java/com/ven/assists/simple/MainActivity.kt
+++ simple/src/main/java/com/ven/assists/simple/MainActivity.kt
@@ -2,7 +2,6 @@ package com.ven.assists.simple
 
 import android.Manifest
 import android.content.Intent
-import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
@@ -79,11 +78,6 @@ class MainActivity : AppCompatActivity(), AssistsServiceListener {
             }
         }
     }
-
-    private var disableNotificationView: View? = null
-
-
-    private lateinit var drawingView: MultiTouchDrawingView
 
     override fun onResume() {
         super.onResume()
